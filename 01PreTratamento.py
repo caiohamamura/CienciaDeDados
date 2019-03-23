aaaf45a4f68ce9b55b09c363b665c5b5a686f702
caioh <caioh@DESKTOP-RB1K0R8>
2019-03-22 21:41:11 -0300

    Ajuste de modelo randomForestRegressor para teste

--- 01PreTratamento.py
+++ 01PreTratamento.py
@@ -41,8 +41,42 @@ ordinalColumns=pd.notnull(varDataFrame.Categories)
 varDataFrame.Categories = varDataFrame.Categories[ordinalColumns].map(eval)
 for col, cats in varDataFrame[ordinalColumns][["Name", "Categories"]].values:
     dataFrame[col] = pd.Categorical(dataFrame[col], categories=cats, ordered=True)
+    
+for col, cats in varDataFrame[ordinalColumns][["Name", "Categories"]].values:
+    dataFrame[col] = dataFrame[col].cat.rename_categories(range(dataFrame[col].cat.categories.size))
 
 
 # Checar dados faltantes
 import missingno as msno
 msno.matrix(dataFrame, labels=True, color=(0.5,0.5,1), sparkline=False)
+
+fig1, ax1 = plt.subplots()
+ax1.set_title('Basic Plot')
+plt.boxplot([dataFrame["SalePrice"][dataFrame.PoolArea>0], dataFrame["SalePrice"][dataFrame.PoolArea==0]], 
+            labels=["Com Piscina", "Sem Piscina"])
+
+import sklearn.ensemble
+from sklearn import preprocessing
+
+le = preprocessing.LabelEncoder()
+
+tudo=np.logical_not(nominalColumns)
+
+dataFrame2 = dataFrame[dataFrame.columns.values[tudo.values.astype(bool)]]
+dataFrame2=dataFrame2.fillna(0)
+cols = list(dataFrame2.columns)
+colPredict = "SalePrice"
+cols.remove(colPredict)
+rf = sklearn.ensemble.RandomForestRegressor()
+rf.fit(dataFrame2[cols], dataFrame2[colPredict])
+plt.xlabel("predict")
+plt.ylabel("observed")
+plt.plot(rf.predict(dataFrame2[cols]),dataFrame2[colPredict], 'ro')
+plt.show()
+
+dados=pd.DataFrame({"coluna":dataFrame2.columns[:-1], "importancia": rf.feature_importances_})
+dados=dados.sort_values(by="importancia", ascending=False)
+plt.xticks(rotation='vertical')
+plt.rcParams["figure.figsize"] = (15,3)
+plt.plot(dados.iloc[:,0], dados.iloc[:,1])
+plt.show()
