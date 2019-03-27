fc5d1309825182e24ee9a82a0ceb825de20c201e
caioh <caioh@DESKTOP-RB1K0R8>
2019-03-27 19:24:22 -0300

    Violin plot da qualidade geral

--- 01PreTratamento.py
+++ 01PreTratamento.py
@@ -5,12 +5,19 @@ Spyder Editor
 Este é um arquivo de script temporário.
 """
 
+#%%
+import missingno as msno
 import pandas as pd
-#import clipboard
+# Importa as bibliotecas de Machine Learning
+import sklearn.ensemble
 from matplotlib import pyplot as plt
+from matplotlib.pyplot import figure
+from sklearn import preprocessing
 
 # Numpy by Pandas
 np = pd.np
+
+#%%
 # Lê arquivo CSV de Treino
 dataFrame=pd.read_csv('train.csv')
 
@@ -25,6 +32,7 @@ dataFrame=pd.read_csv('train.csv')
 # Remover coluna Id (nao utiliza na análise)
 dataFrame = dataFrame.drop("Id", 1)
 
+#%%
 # =============================================================================
 # Ajustar colunas QUALITATIVAS: nominais/categóricas ou ordinais
 # =============================================================================
@@ -37,9 +45,8 @@ nominalColumns=np.logical_and(varDataFrame.Categorical, np.logical_not(varDataFr
 
 # Nominais/categóricas
 for col in varDataFrame.Name[nominalColumns]:
-    #print(col)
-    print(pd.Categorical(dataFrame[col], ordered=False))
-    #dataFrame[col] = pd.Categorical(dataFrame[col], ordered=False)
+    dataFrame[col] = pd.Categorical(dataFrame[col], ordered=False)
+
 
 # Ordinais
 # Busca as categorias ordinais não-nulas
@@ -53,27 +60,24 @@ for col, cats in varDataFrame[ordinalColumns][["Name", "Categories"]].values:
 for col, cats in varDataFrame[ordinalColumns][["Name", "Categories"]].values:
     dataFrame[col] = dataFrame[col].cat.rename_categories(range(dataFrame[col].cat.categories.size))
 
+#%%
 # =============================================================================
-
 # Checar dados faltantes em modo gráfico
-import missingno as msno
+# =============================================================================
 msno.matrix(dataFrame, labels=True, color=(0.5,0.5,1), sparkline=False)
 
+#%%
 # Verificar graficamente valor de venda da casa com e sem piscina
 fig1, ax1 = plt.subplots()
 ax1.set_title('Basic Plot')
 plt.boxplot([dataFrame["SalePrice"][dataFrame.PoolArea>0], dataFrame["SalePrice"][dataFrame.PoolArea==0]], 
             labels=["Com Piscina", "Sem Piscina"])
 
-# =============================================================================
-
-# Importa as bibliotecas de Machine Learning
-import sklearn.ensemble
-from sklearn import preprocessing
+#%%
 le = preprocessing.LabelEncoder()
-
 # Ignora as 23 variáveis nominais/categóricas
-tudo=np.logical_not(nominalColumns)
+tudo=np.logical_or(np.logical_not(nominalColumns), varDataFrame.Name == "Neighborhood")
+
 
 # Cria novo conjunto de dados sem as 23 variáveis nominais/categóricas
 dataFrame2 = dataFrame[dataFrame.columns.values[tudo.values.astype(bool)]]
@@ -90,14 +94,21 @@ colPredict = "SalePrice"
 # Remove a variável a ser predita
 cols.remove(colPredict)
 
+
+#%%
 # Realiza a regressão com o algoritmo floresta aleatória e plota os valores encontrados x esperados
 rf = sklearn.ensemble.RandomForestRegressor()
 rf.fit(dataFrame2[cols], dataFrame2[colPredict])
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
 plt.xlabel("predict")
 plt.ylabel("observed")
 plt.plot(rf.predict(dataFrame2[cols]),dataFrame2[colPredict], 'ro')
+ax.set_facecolor("#1E1E1E")
 plt.show()
 
+#%%
 # Plota as variáveis mais importantes da regressão em ordem decrescente
 dados=pd.DataFrame({"coluna":dataFrame2.columns[:-1], "importancia": rf.feature_importances_})
 dados=dados.sort_values(by="importancia", ascending=False)
@@ -106,7 +117,7 @@ plt.rcParams["figure.figsize"] = (15,3)
 plt.plot(dados.iloc[:,0], dados.iloc[:,1])
 plt.show()
 
-
+#%%
 # Histograma de Preço de venda
 plt.xlabel("Preço de Venda (US$)")
 plt.ylabel("Quantidade")
@@ -114,17 +125,38 @@ plt.axes().set_xlim(0, 500000)
 plt.hist(dataFrame2["SalePrice"], bins=40)
 plt.show()
 
-
+#%%
 # Histogramas de preço de venda por bairro
-from matplotlib.pyplot import figure
-figure(num=None, figsize=(15, 10), dpi=80, facecolor='w', edgecolor='k')
-for i, bairro in enumerate(np.unique(dataFrame["Neighborhood"])):
+figure(num=None, 
+        figsize=(15, 10), 
+        dpi=80, edgecolor='k')
+bairros = dataFrame["Neighborhood"]
+precoDeVenda = dataFrame["SalePrice"]
+for i, bairro in enumerate(
+            np.unique(bairros)):
     axs = plt.subplot(5, 5, i+1)
     axs.set_xlim(0, 500000)
     axs.set_ylim(0, 45)
     plt.xticks([0, 250000, 500000])
-    plt.hist(dataFrame[dataFrame["Neighborhood"] == bairro]["SalePrice"], bins='auto', alpha=0.5, label='yes')   
+    plt.hist(
+        precoDeVenda[bairros == bairro], 
+        bins='auto', alpha=0.5, label='yes')
     plt.title(bairro)   
 plt.silent_list
 plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
 plt.show()
+
+#%%
+diff = abs(rf.predict(dataFrame2[cols])-dataFrame2[colPredict])
+dataFrame2[colPredict].iloc[523]
+rf.predict(dataFrame2[cols])[523]
+
+#%%
+diff = diff.sort_values(ascending = False)
+
+#%%
+from sklearn.preprocessing import OneHotEncoder
+coder = OneHotEncoder(handle_unknown="ignore")
+
+#%%
+# Explorando as 
