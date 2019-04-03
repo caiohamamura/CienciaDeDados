145099ea32dcdd81402a3d86194686bcf15ffbdb
caioh <caioh@DESKTOP-RB1K0R8>
2019-04-03 18:01:54 -0300

    Tensor flow

--- 02TensorFlor.py
+++ 02TensorFlor.py
@@ -1,17 +1,20 @@
 from mpi4py import MPI
 import numpy as np
+import socket
+hostname = list(socket.gethostname().encode('utf8'))
+
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
-numDataPerRank = 10
-sendbuf = np.arange(numDataPerRank*rank, numDataPerRank*(rank+1), dtype=np.float64)
+numDataPerRank = len(hostname)
+sendbuf = np.array(hostname, dtype=np.uint8)
 print('Rank: ',rank, ', sendbuf: ',sendbuf)
 
 recvbuf = None
 if rank == 0:
-    recvbuf = np.empty(numDataPerRank*size, dtype=np.float64)
+    recvbuf = np.empty(numDataPerRank*size, dtype=np.uint8)
 
 comm.Gather(sendbuf, recvbuf, root=0)
 
