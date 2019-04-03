d2b335ec53bc906a93a949082a02ff1b1d2ac499
caioh <caioh@DESKTOP-RB1K0R8>
2019-04-03 18:37:46 -0300

    Tensor flow

--- 02TensorFlor.py
+++ 02TensorFlor.py
@@ -1,6 +1,8 @@
 from mpi4py import MPI
 import numpy as np
 import socket
+import tensorflow as tf
+
 hostname = list(socket.gethostname().encode('utf8'))
 
 
@@ -26,5 +28,26 @@ result = np.split(recvbuf, size)
 result = [bytes(list(i)).decode('utf8') for i in result]
 print('This rank ', rank,' Ranks: ',recvbuf2, ', recvbuf received: ', result)
 
-# hosts = result
-# ranks = recvbuf2
+hosts = result
+ranks = recvbuf2
+
+hosts = ['s001-n002', 's001-n002', 's001-n095', 's001-n095', 's001-n004', 's001-n004', 's001-n096', 's001-n096']
+ranks = range(7)
+
+jobs = {nodes: '%s:222%s' % (i, j) for (i,j) in zip(hosts, ranks)]}
+cluster = tf.train.ClusterSpec(jobs)
+
+server = tf.train.Server(cluster, job_name="local", task_index=rank)
+tf.reset_default_graph()
+var = tf.Variable(initial_value=0.0, name='var')
+sess = tf.Session(server.target)
+sess.run(tf.global_variables_initializer())
+print("Initial value of var in session %d:" % (rank), sess.run(var))
+
+if rank == 0:
+    sess.run(var.assign_add(1.0))
+
+
+print("Value of var in session %d:" % (rank), sess.run(var))
+
+
